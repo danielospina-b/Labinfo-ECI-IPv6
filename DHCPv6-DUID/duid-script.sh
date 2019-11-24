b1e6529836260f74231a1a44c0781ca45150eab4
Daniel Ospina <daniel.ospina-b@mail.escuelaing.edu.co>
2019-11-24 18:13:45 -0500

    Improved Linux Script

--- DHCPv6-DUID/duid-script.sh
+++ DHCPv6-DUID/duid-script.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 ## Author: Daniel Ospina GitHub: danielospina-b
 
+# Set here the ADAPTER to extract MAC address
+ADAPTER=eno1
+
 # Make sure only root can run our script
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
@@ -12,23 +15,26 @@ echo "\n········ IPv6 DUID Configuration Script ········"
 echo "\nThis file contains comments, in case the script does not work as expected...\n"
 
 # Prints a list of all MAC Addresses using ip addr show, then picks the first one
-echo "Printing all MAC Addresses..."
+echo "Printing all adapters and MAC Addresses..."
 echo "============================="
-ip addr show | grep -Po 'link/ether \K.*$' | awk '{print $1;}'
+ip addr show | grep -B 1 -P 'link/ether \K.*$' | awk '{print $2;}'
+echo "============================="
+echo "Set the correct adapter name in line 5 of this script"
 echo "=============================\n"
 
 # grep gets the line that shows the MAC Address from a 'ethernet' interface 
 # and awk gets the first line ("NR==1")  and the first column ("print $1;")
-MAC_ADDRESS=$(ip addr show | grep -Po 'link/ether \K.*$' | awk 'NR==1{print $1;}')
+MAC_ADDRESS=$(ip addr show | grep -A 1 $ADAPTER: | grep -Po 'link/ether \K.*$' | awk 'NR==1{print $1;}')
 # If the script does not use the correct MAC address 
 # change 'NR==1' to 'NR=2' to use the second MAC addres from the list...
 
+echo Selected adapter: $ADAPTER
 echo MAC Address used in DUID: $MAC_ADDRESS
 
 # Removes previous dhcp6 client configuration file and creates a new file with the new DUID value.
 rm -rf /etc/dhcp/dhclient6.conf
 touch /etc/dhcp/dhclient6.conf
-echo interface "eno1" { >> /etc/dhcp/dhclient6.conf
+echo interface $ADAPTER { >> /etc/dhcp/dhclient6.conf
 echo "    " send dhcp6.client-id 00:01:00:01:ec:10:ec:10:$MAC_ADDRESS\; >> /etc/dhcp/dhclient6.conf
 echo } >> /etc/dhcp/dhclient6.conf
 
